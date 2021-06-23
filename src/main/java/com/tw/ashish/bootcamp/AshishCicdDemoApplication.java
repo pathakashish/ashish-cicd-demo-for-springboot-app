95b2440a51b0e168f0ec69faebd6a20db7eaed74
Ashish Pathak <ashish.pathak@thoughtworks.com>
2021-06-23 22:13:34 +0530

    Add mapping for homepage

--- src/main/java/com/tw/ashish/bootcamp/AshishCicdDemoApplication.java
+++ src/main/java/com/tw/ashish/bootcamp/AshishCicdDemoApplication.java
@@ -2,7 +2,10 @@ package com.tw.ashish.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AshishCicdDemoApplication {
 
@@ -10,4 +13,8 @@ public class AshishCicdDemoApplication {
 		SpringApplication.run(AshishCicdDemoApplication.class, args);
 	}
 
+	@GetMapping(value = "/")
+	public String homepage() {
+		return "<h1>My fancy homepage!</h1>";
+	}
 }
