c6b0c4a13661aa98f4f6bbe68c716c4efa3e11c3
Ashish Pathak <ashish.pathak@thoughtworks.com>
2021-06-23 22:09:33 +0530

    Initial commit

---
+++ src/main/java/com/tw/ashish/bootcamp/AshishCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tw.ashish.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshishCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshishCicdDemoApplication.class, args);
+	}
+
+}
